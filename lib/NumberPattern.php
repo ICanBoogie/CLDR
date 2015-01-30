cbd301949b2f2daa74264a233f4357bb0d451b8e
Olivier Laviale <olivier.laviale@gmail.com>
2015-01-30 03:22:07 +0100

    Refactored NumberFormatter::format()

--- lib/NumberPattern.php
+++ lib/NumberPattern.php
@@ -207,4 +207,83 @@ class NumberPattern
 	{
 		return $this->pattern;
 	}
+
+	/**
+	 * Parse a number according to the pattern and return its integer and decimal parts.
+	 *
+	 * @param number $number
+	 *
+	 * @return array An array made with the integer and decimal parts of the number.
+	 */
+	public function parse_number($number)
+	{
+		$number = abs($number * $this->multiplier);
+
+		if ($this->max_decimal_digits >= 0)
+		{
+			$number = round($number, $this->max_decimal_digits);
+		}
+
+		$number = "$number";
+
+		if (($pos = strpos($number, '.')) !== false)
+		{
+			return [ substr($number, 0, $pos), substr($number, $pos + 1) ];
+		}
+
+		return [ $number, '' ];
+	}
+
+	/**
+	 * Formats integer according to group pattern.
+	 *
+	 * @param int $integer
+	 * @param string $group_symbol
+	 *
+	 * @return string
+	 */
+	public function format_integer_with_group($integer, $group_symbol)
+	{
+		$integer = str_pad($integer, $this->integer_digits, '0', STR_PAD_LEFT);
+		$group_size1 = $this->group_size1;
+
+		if ($group_size1 < 1 || strlen($integer) <= $this->group_size1)
+		{
+			return $integer;
+		}
+
+		$group_size2 = $this->group_size2;
+
+		$str1 = substr($integer, 0, -$group_size1);
+		$str2 = substr($integer, -$group_size1);
+		$size = $group_size2 > 0 ? $group_size2 : $group_size1;
+		$str1 = str_pad($str1, (int) ((strlen($str1) + $size - 1) / $size) * $size, ' ', STR_PAD_LEFT);
+
+		return ltrim(implode($group_symbol, str_split($str1, $size))) . $group_symbol . $str2;
+	}
+
+	/**
+	 * Formats an integer with a decimal.
+	 *
+	 * @param string|int $integer An integer, or a formatted integer as returned by
+	 * {@link format_integer_with_group}.
+	 * @param string $decimal
+	 * @param string $decimal_symbol
+	 *
+	 * @return string
+	 */
+	public function format_integer_with_decimal($integer, $decimal, $decimal_symbol)
+	{
+		if ($this->decimal_digits > strlen($decimal))
+		{
+			$decimal = str_pad($decimal, $this->decimal_digits, '0');
+		}
+
+		if (strlen($decimal))
+		{
+			$decimal = $decimal_symbol . $decimal;
+		}
+
+		return "$integer" . $decimal;
+	}
 }
